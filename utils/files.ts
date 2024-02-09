6e358763685073d6df81175c967a4de71404ff4f
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-02-09 12:49:57 -0500

    chore: moved content to content dir
    Signed-off-by: Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>

--- utils/files.ts
+++ utils/files.ts
@@ -1,11 +1,11 @@
 async function getGreeting() {
-  const greetingFile = await Bun.file("greeting.txt").text();
+  const greetingFile = await Bun.file("content/greeting.txt").text();
   const greetings = greetingFile.split("\n");
   return greetings[Math.floor(Math.random() * greetings.length)];
 }
 
 export async function getMessage() {
-  let messageFile = await Bun.file("message.txt").text();
+  let messageFile = await Bun.file("content/message.txt").text();
   messageFile = messageFile.replace("{greeting}", await getGreeting());
   return messageFile;
 }
